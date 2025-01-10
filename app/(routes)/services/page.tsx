c0533c4aeef6513c74a303c8dac98b876b923f8a
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2025-01-09 19:04:11 -0700

    first commit

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -2,6 +2,7 @@ import AvatarServices from "@/components/avatar-services";
 import CircleImage from "@/components/circle-image";
 import SliderServices from "@/components/slider-services";
 import TransitionPage from "@/components/transition-page";
+import Link from "next/link";
 
 const ServicesPage = () => {
     return (
@@ -15,7 +16,7 @@ const ServicesPage = () => {
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
                     <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de electricidad especializados en la instalación, mantenimiento y reparación de sistemas eléctricos. Con un enfoque en seguridad y eficiencia, utilizo las últimas tecnologías y herramientas del sector para garantizar que todas las instalaciones eléctricas cumplan con las normativas vigentes. Mi objetivo es proporcionar soluciones eléctricas fiables y de alta calidad que aseguren el correcto funcionamiento y la seguridad de los hogares y negocios de mis clientes, mejorando su bienestar y tranquilidad..</p>
-                    <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
+                    <Link href="http://wa.me/qr/FKW5AV6OHQHUM" target="_blank"  className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</Link>
                 </div>
 
                 {/* SLIDER */}

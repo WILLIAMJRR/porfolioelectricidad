71e2ae3f6d7df46a5e8e72ef6c63d2af74046fb3
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2025-01-09 18:52:59 -0700

    Primer commit

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -11,7 +11,7 @@ const ServicesPage = () => {
             <CircleImage />
             <AvatarServices />
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
-                <div className="max-w-[450px]">
+                <div className="max-w-[450px] mt-32">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
                     <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de electricidad especializados en la instalación, mantenimiento y reparación de sistemas eléctricos. Con un enfoque en seguridad y eficiencia, utilizo las últimas tecnologías y herramientas del sector para garantizar que todas las instalaciones eléctricas cumplan con las normativas vigentes. Mi objetivo es proporcionar soluciones eléctricas fiables y de alta calidad que aseguren el correcto funcionamiento y la seguridad de los hogares y negocios de mis clientes, mejorando su bienestar y tranquilidad..</p>
